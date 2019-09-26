7a94fc5e8fe5f6a10fbece5b3db2126ff6eb070c
ShneorNagar <shneor30@gmail.com>
2019-09-26 14:49:37 +0300

    fix inCart bug

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    loadInCartHolders();
     loadItems();
     $(document).on("click", ".add-to-cart", function (e) {
         var row = $(this).parent().parent();
@@ -6,6 +7,9 @@ $(document).ready(function () {
         
         var itemStr = localStorage.getItem(id);
         var item = JSON.parse(itemStr);
+        
+        addToInCart(item.id, true);
+        
         console.log(item);
         if (!item.inCart) {
             item.timesAdded++;
@@ -27,6 +31,7 @@ function loadItems() {
 
 // Adds item to items table and to localstorage.
 function addItemForSale(item) {
+
     var row = $(".templates .item-row").clone();
     var shekels = item.price * 4;
     row.find(".item-id").html(item.id);
@@ -34,13 +39,24 @@ function addItemForSale(item) {
     row.find(".item-price").html(item.price + "$<br>" + shekels + "&#8362");
     row.find(".item-state").html(item.state);
     row.find(".item-image img").attr("src", item.imgUrl);
-    item.inCart = false;
-    item.timesAdded = 0;
     $("#items tbody").append(row);
     
+
     addItemToLocalStorage(item);
 }
 
 function addItemToLocalStorage(item) {
     localStorage.setItem(item.id, JSON.stringify(item));
+}
+
+function loadInCartHolders(){
+    var inCartIds = localStorage.getItem("inCartIds") || "[]";
+    localStorage.setItem("inCartIds", inCartIds);
+}
+
+function addToInCart(id){
+    var inCartIdsStr = localStorage.getItem("inCartIds") || "[]";
+    var inCartIds = JSON.parse(inCartIdsStr);
+    inCartIds.push(id);
+    localStorage.setItem("inCartIds", JSON.stringify(inCartIds));
 }
---
+++ js/shpooingCart.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    loadItemsFromLocalStorage();
+});
+
+function loadItemsFromLocalStorage(){
+    allStorage().forEach(function(item){
+        var jsonStr = JSON.parse(item);
+        if(jsonStr["inCart"]){
+            addItemForSale(jsonStr);
+        }
+    })
+}
+
+function allStorage() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]) );
+    }
+
+    return values;
+}
+
+function addItemForSale(item) {
+    console.log(item);
+    var row = $(".templates .item-row").clone();
+    var shekels = item.price * 4;
+    row.find(".item-id").html(item["id"]);
+    console.log(item["id"]);
+    row.find(".item-name").html(item["name"]);
+    console.log(item["name"]);
+    row.find(".item-price").html(item["price"] + "$<br>" + shekels + "&#8362");
+    console.log(item["price"]);
+    row.find(".item-state").html(item["state"]);
+    console.log(item["state"]);
+    row.find(".item-image img").attr("src", item["imgUrl"]);
+    console.log(item["imgUrl"]);
+    $("#items tbody").append(row);
+}
